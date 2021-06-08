c27d4aab5b26df3494911bf9071661f0b8952fd4
Isabella Sandoval <isabella.sandoval95@gmail.com>
2021-06-08 12:50:09 -0600

    rspec search model

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Search.new(title: "Becoming", author: "Michelle Obama")}
+  before { subject.save }
+
+
+  describe 'title' do
+    it 'must be present' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end 
+  end
+
+  describe 'author' do
+    it 'must be present' do
+      subject.author = nil
+      expect(subject).to_not be_valid
+    end 
+  end
+
 end
