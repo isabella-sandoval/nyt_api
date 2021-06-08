1023e01b3ad94034703029882750580bd7332204
Isabella Sandoval <isabella.sandoval95@gmail.com>
2021-06-08 13:22:28 -0600

    rspec search requests

--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Searches", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+  
+    it 'returns all nyt bestsellers' do
+        get '/searches'
+
+        expect(response).to have_http_status(:success)
+    end
+
+    it 'returns select nyt bestseller based off of id' do
+        get '/searches/2'
+
+        expect(response).to have_http_status(:success)
+    end
+
+    it 'returns nyt best sellers based off params' do
+        get "/fetch_data", :params => { :author => "Michelle Obama"}
+
+        expect(response).to have_http_status(:success)
+    end
 end
