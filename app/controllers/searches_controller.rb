394757ba1a661cc55c78e974abc20d407e0ee91f
Isabella Sandoval <isabella.sandoval95@gmail.com>
2021-06-08 12:25:44 -0600

    searches controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,56 @@
+class SearchesController < ApplicationController
+
+def index
+    @searches = Search.all
+    render json: @searches
+end
+
+def show
+    @search = Search.find_by(id: params[:id])
+    render json: @search
+end
+
+def fetch_data
+    params.permit(:author, :publisher, :description, :title, :sort)
+
+    sql_string = ""
+    querie_string = ""
+    sort_by = params[:sort] ? params[:sort].downcase : "title"
+
+    if(params[:author].present?)
+      sql_string += " OR "if(sql_string.length > 1)
+      sql_string += "lower(author) LIKE '%#{params[:author].downcase}%'"
+
+      querie_string += "&"if(sql_string.length > 1)
+      querie_string += "author=#{params[:author].downcase}"
+    end
+
+    if(params[:publisher].present?)
+      sql_string += " OR "if(sql_string.length > 1)
+      sql_string += "lower(publisher) LIKE '%#{params[:publisher].downcase}%'"
+
+      querie_string += "&"if(sql_string.length > 1)
+      querie_string += "publisher=#{params[:publisher].downcase}"
+    end
+
+    sql_string = "SELECT * FROM searches WHERE #{sql_string} ORDER BY #{sort_by}"
+    results = Search.find_by_sql(sql_string)
+
+      #check db
+    if results.present?
+        # debugger
+        render json: results
+    else
+    #fetch from api 
+    response = RestClient.get("https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?#{querie_string}&api-key=#{ENV["API_KEY"]}")
+    books_array = JSON.parse(response)["results"]
+
+    books_array.each do |book|
+        Search.create(title: book["title"], description: book["description"], author: book["author"], publisher: book["publisher"])
+    end
+        render json: response
+    end
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :searches
+  get :fetch_data, to: "searches#fetch_data"
+
+  # root 'searches#index'
+
 end
